bb567b171457f3058ccd634626d9e22e7a0139e5
DenisMurashov <99132592+DenisMurashov@users.noreply.github.com>
2022-04-11 01:39:39 +0700

    Update hello.c

--- src/libhello/hello.c
+++ src/libhello/hello.c
@@ -1,20 +1,28 @@
 #include <libhello/hello.h>
-void make_string_null(char c[]) {
-  int i;
-  for (i = 0; i < strlen(c); i++) {
-    c[i] = ' ';
-  }
-}
-bool num_check(char c[]) {
-  int i, k = 0;
-  for (i = ukazatel(c, '(', 0); i < ukazatel(c, ')', 0); i++) {
-    if (!isdigit(c[i]) && !isspace(c[i]) && !ispunct(c[i])) {
-      k++;
+void make_string_null(char c[])
+{
+    int i;
+    for (i = 0; i < strlen(c); i++)
+    {
+        c[i] = ' ';
     }
-  }
-  if (k > 0) {
-    return false;
-  } else {
-    return true;
-  }
+}
+bool num_check(char c[])
+{
+    int i, k = 0;
+    for (i = ukazatel(c, '(', 0); i < ukazatel(c, ')', 0); i++)
+    {
+        if (!isdigit(c[i]) && !isspace(c[i]) && !ispunct(c[i]))
+        {
+            k++;
+        }
+    }
+    if (k > 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
