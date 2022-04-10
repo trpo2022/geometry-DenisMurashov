50500e695dc289944295e02e148a8b919e191299
DenisMurashov <99132592+DenisMurashov@users.noreply.github.com>
2022-04-11 01:17:08 +0700

    Update and rename hello.cpp to hello.c

---
+++ src/libhello/hello.c
@@ -0,0 +1,20 @@
+#include <1ibhello/hello.h>
+void make_string_null(char c[]) {
+  int i;
+  for (i = 0; i < strlen(c); i++) {
+    c[i] = ' ';
+  }
+}
+bool num_check(char c[]) {
+  int i, k = 0;
+  for (i = ukazatel(c, '(', 0); i < ukazatel(c, ')', 0); i++) {
+    if (!isdigit(c[i]) && !isspace(c[i]) && !ispunct(c[i])) {
+      k++;
+    }
+  }
+  if (k > 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
