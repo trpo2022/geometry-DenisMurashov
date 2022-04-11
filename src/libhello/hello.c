02d19ab4cc94fec4ba00b015f1f1e8ebf87af8e8
DenisMurashov <99132592+DenisMurashov@users.noreply.github.com>
2022-04-11 10:56:09 +0700

    Update hello.c

--- src/libhello/hello.c
+++ src/libhello/hello.c
@@ -1,69 +1,81 @@
-#include <libgeometry/functions.h>
-#include <stdio.h>
-int ukazatel(char c[], char p, int i)
+#include <1ibhello/hello.h>
+void Circle(int F, int CH, int iCnt, int T, int Zap)
 {
-    int ukaz = 0;
-    while (c[i] != '\0')
-    {
-        if (c[i] == p)
-        {
-            ukaz = i;
-            break;
+    if (F > 0) {
+        int err = 0;
+
+        if (CH == 0) {
+            iCnt = iCnt - CH;
+            if (iCnt > 6 || iCnt < 6) {
+                printf("Error at column 0: expected 'circle', 'triangle' "
+                       "or "
+                       "'polygon'\n\n");
+                err++;
+            }
         }
-        i++;
-    }
-    return ukaz;
-}
-int countelement(char c[], char p)
-{
-    int count = 0;
-    int i = 0;
-    while (c[i] != '\0')
-    {
-        if (c[i] == p)
-        {
-            count++;
+
+        if (T >= 1) {
+            printf("Error at column 21: expected '.'\n\n");
+            err++;
         }
-        i++;
-    }
-    return count;
-}
-double side(double x1, double y1, double x2, double y2)
-{
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-}
-double area_of_circle(double radius) { return pi * pow(radius, 2); }
-double area_of_triangle(double a, double b, double c)
-{
-    double halfperimeter = (a + b + c) / 2;
-    double area = sqrt(halfperimeter * (halfperimeter - a) * (halfperimeter - b) *
-                       (halfperimeter - c));
-    return area;
-}
-void make_string_null(char c[])
-{
-    int i;
-    for (i = 0; i < strlen(c); i++)
-    {
-        c[i] = ' ';
-    }
-}
-bool num_check(char c[])
-{
-    int i, k = 0;
-    for (i = ukazatel(c, '(', 0); i < ukazatel(c, ')', 0); i++)
-    {
-        if (!isdigit(c[i]) && !isspace(c[i]) && !ispunct(c[i]))
-        {
-            k++;
+        if (Zap >= 1) {
+            printf("Error at column 20: expected ','\n\n");
+            err++;
         }
+        if (CH >= 1) {
+            printf("Error at column 7: expected '<double>'\n\n");
+            err++;
+        }
+
+        if (err == 0) {
+            double Area = 3.14 * pow(a[2], 2);
+            cout << "Area: " << Area << "\n";
+            double perimeter = 2 * 3.14 * a[2];
+            cout << "Perimeter: " << perimeter << "\n\n";
+        }
+        cout << "--------------------------------------------------------------"
+                "--------------------\n";
     }
-    if (k > 0)
-    {
-        return false;
-    }
-    else
-    {
-        return true;
+}
+
+void Triangle(int F1, int CH, int iCnt1, int T, int Zap)
+{
+    if (F1 > 0) {
+        int err = 0;
+        if (CH == 0) {
+            iCnt1 = iCnt1 - CH;
+            if (iCnt1 > 8 || iCnt1 < 8) {
+                printf("Error at column 0: expected 'circle', 'triangle' "
+                       "or "
+                       "'polygon'\n\n");
+                err++;
+            }
+        }
+        if (T >= 1) {
+            printf("Error at column 21: expected '.'\n\n");
+            err++;
+        }
+        if (Zap >= 1) {
+            printf("Error at column 20: expected ','\n\n");
+            err++;
+        }
+        if (CH > 1) {
+            printf("Error at column 7: expected '<double>'\n\n");
+            err++;
+        }
+
+        if (err == 0) {
+            double Area = sqrt(pow((a[0] - a[2]), 2) + pow((a[1] - a[3]), 2));
+            cout << "Area: " << Area << '\n';
+
+            double SideAB = sqrt(pow((a[2] - a[0]), 2) + pow((a[3] - a[1]), 2));
+            double SideBC = sqrt(pow((a[4] - a[2]), 2) + pow((a[5] - a[3]), 2));
+            double SideAC = sqrt(pow((a[4] - a[0]), 2) + pow((a[5] - a[1]), 2));
+
+            double perimeter = SideAB + SideBC + SideAC;
+            cout << "Perimeter: " << perimeter << '\n';
+        }
+        cout << "--------------------------------------------------------------"
+                "--------------------\n";
     }
 }
