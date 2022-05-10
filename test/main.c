44b0e68cdd71d3eb83d9919849fa08086217e838
DenisMurashov <99132592+DenisMurashov@users.noreply.github.com>
2022-05-10 22:52:38 +0700

    Update main.c

--- test/main.c
+++ test/main.c
@@ -1,8 +1,7 @@
+#define CTEST_MAIN
 #include "../thirdparty/ctest.h"
-#include "../src/libgeometry/hello.c"
-#include <math.h>
 
 int main(int argc, const char** argv)
 {
-    return CTEST(SUM_test, result_sum);
+   return ctest_main(argc,argv);
 }
