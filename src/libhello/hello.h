3181188bb67956e512730e1fec8f16fda97d19f8
DenisMurashov <99132592+DenisMurashov@users.noreply.github.com>
2022-05-10 21:58:46 +0700

    Update hello.h

--- src/libhello/hello.h
+++ src/libhello/hello.h
@@ -1,3 +1,8 @@
 #pragma once
+A(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double Dx, double Dy);
+AreaCircle();
+PerCircle();
 Circle(F, CH, iCnt, T, Zap);
+AreaTriangle();
+PerTriangle();
 Triangle(F1, CH, iCnt1, T, Zap);
